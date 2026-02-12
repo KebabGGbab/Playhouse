df232910b94280c0b8728e74d210d0d69a2fb22c
artem <kebabggbab@gmail.com>
2026-02-13 01:43:06 +0700

    Создан класс MainWindowViewModel, который включает в себя все ViewModel, требуемые MainWindow.

---
+++ Playhouse.ViewModels/ViewModels/MainWindowViewModels.cs
@@ -0,0 +1,28 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Playhouse.ViewModels.ViewModels
+{
+    public class MainWindowViewModels : ObservableObject
+    {
+        public RunViewModel RunViewModel { get; }
+
+        public BotsInfoViewModel BotsInfoViewModel { get; }
+
+        public BrowserProfilesViewModel BrowserProfilesViewModel { get; }
+
+        public SettingsViewModel SettingsViewModel { get; }
+
+        public MainWindowViewModels(RunViewModel runVM, BotsInfoViewModel botsVM, BrowserProfilesViewModel profilesVM, SettingsViewModel settingsVM)
+        {
+            ArgumentNullException.ThrowIfNull(runVM, nameof(runVM));
+            ArgumentNullException.ThrowIfNull(botsVM, nameof(botsVM));
+            ArgumentNullException.ThrowIfNull(profilesVM, nameof(profilesVM));
+            ArgumentNullException.ThrowIfNull(settingsVM, nameof(settingsVM));
+
+            RunViewModel = runVM;
+            BotsInfoViewModel = botsVM;
+            BrowserProfilesViewModel = profilesVM;
+            SettingsViewModel = settingsVM;
+        }
+    }
+}
