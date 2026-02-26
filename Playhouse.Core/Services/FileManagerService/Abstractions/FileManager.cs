3215ea9d614a0631721f337c2f6955c6c34d4d29
artem <kebabggbab@gmail.com>
2026-02-27 03:59:21 +0700

    Добавлен сервис для создания и удаления файлов.

---
+++ Playhouse.Core/Services/FileManagerService/Abstractions/FileManager.cs
@@ -0,0 +1,20 @@
+﻿using Playhouse.Core.Services.FilePathResolverService.Abstractions;
+
+namespace Playhouse.Core.Services.FileManagerService.Abstractions
+{
+    public abstract class FileManager<TModel>
+    {
+        protected IFilePathResolver PathResolver { get; }
+
+        protected FileManager(IFilePathResolver pathResolver)
+        {
+            ArgumentNullException.ThrowIfNull(pathResolver, nameof(pathResolver));
+
+            PathResolver = pathResolver;
+        }
+
+        public abstract void Create(TModel model);
+
+        public abstract void Delete(TModel model);
+    }
+}
---
+++ Playhouse.Core/Services/FileManagerService/BrowserProfileFileManager.cs
@@ -0,0 +1,27 @@
+﻿using Playhouse.Core.Models;
+using Playhouse.Core.Services.FileManagerService.Abstractions;
+using Playhouse.Core.Services.FilePathResolverService.Abstractions;
+
+namespace Playhouse.Core.Services.FileManagerService
+{
+    public class BrowserProfileFileManager : FileManager<BrowserProfile>
+    {
+        public BrowserProfileFileManager(IFilePathResolver pathResolver) : base(pathResolver) 
+        {
+        } 
+
+        public override void Create(BrowserProfile model)
+        {
+            ArgumentNullException.ThrowIfNull(model, nameof(model));
+
+            Directory.CreateDirectory(PathResolver.GetPathToDirectoryUserDataDirProfile(model.Id));
+        }
+
+        public override void Delete(BrowserProfile model)
+        {
+            ArgumentNullException.ThrowIfNull(model, nameof(model));
+
+            Directory.Delete(PathResolver.GetPathToDirectoryProfile(model.Id), true);
+        }
+    }
+}
