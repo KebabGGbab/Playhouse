1378e19088450166003a8f08569190ae411ca57a
artem <kebabggbab@gmail.com>
2026-04-12 11:49:44 +0700

    В модуль "Playhouse.Infastructure" были добавлены классы "EnumerableSmartEnumConverter" и "EnumerableValueComparer" для конвертации и отслеживания EF.

---
+++ src/Playhouse.Infrastructure/EnumerableSmartEnumConverter.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq.Expressions;
+using System.Text.Json;
+using Ardalis.SmartEnum;
+using Ardalis.SmartEnum.EFCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace Playhouse.Infrastructure
+{
+    public class EnumerableSmartEnumConverter<TEnum, TValue> : ValueConverter<IEnumerable<TEnum>, string>
+        where TEnum : SmartEnum<TEnum, TValue> 
+        where TValue : IEquatable<TValue>, IComparable<TValue>
+    {
+        private static TValue GetEnumValue(TEnum @enum)
+        {
+            return @enum.Value;
+        }
+
+        public static string ConvertToJson(IEnumerable<TEnum> enums)
+        {
+            IEnumerable<TValue> values = enums.Select(GetEnumValue);
+
+            return JsonSerializer.Serialize(values);
+        }
+
+        public static IEnumerable<TEnum> ConvertFromJson(string json)
+        {
+            IEnumerable<TValue>? values = JsonSerializer.Deserialize<IEnumerable<TValue>>(json);
+
+            if (values == null)
+            {
+                return [];
+            }
+
+            return values.Select(SmartEnumConverter<TEnum, TValue>.GetFromValue);
+        }
+
+        public EnumerableSmartEnumConverter()
+            : base(enums => ConvertToJson(enums), value => ConvertFromJson(value))
+        {
+        }
+    }
+}
