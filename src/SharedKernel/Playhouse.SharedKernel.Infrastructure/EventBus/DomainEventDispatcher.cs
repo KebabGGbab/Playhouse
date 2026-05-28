18a706e21742bc92d631a5cd8876836ab176d23f
artem <kebabggbab@gmail.com>
2026-05-28 21:17:16 +0700

    feat(shared-infrastructure): add DomainEventDispatcher class implementing IDomainEventDispatcher

---
+++ src/SharedKernel/Playhouse.SharedKernel.Infrastructure/EventBus/DomainEventDispatcher.cs
@@ -0,0 +1,33 @@
+﻿using Playhouse.SharedKernel.Domain.Events;
+
+namespace Playhouse.SharedKernel.Infrastructure.EventBus
+{
+    public class DomainEventDispatcher : IDomainEventDispatcher
+    {
+        private readonly HashSet<object> _handlers;
+
+        public DomainEventDispatcher()
+        {
+            _handlers = [];
+        }
+
+        public void AddEventHandler<TEvent>(IDomainEventHandler<TEvent> eventHandler) 
+            where TEvent : IDomainEvent
+        {
+            ArgumentNullException.ThrowIfNull(eventHandler);
+
+            _handlers.Add(eventHandler);
+        }
+
+        public void Dispatch<TEvent>(TEvent domainEvent) 
+            where TEvent : IDomainEvent
+        {
+            ArgumentNullException.ThrowIfNull(domainEvent);
+
+            foreach (IDomainEventHandler<TEvent> handler in _handlers.OfType<IDomainEventHandler<TEvent>>())
+            {
+                handler.Handle(domainEvent);
+            }
+        }
+    }
+}
