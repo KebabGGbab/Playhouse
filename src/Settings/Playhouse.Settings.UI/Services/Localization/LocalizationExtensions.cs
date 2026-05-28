c3691c03c56cc8086ff648b5f464447860499276
artem <kebabggbab@gmail.com>
2026-05-28 20:46:29 +0700

    feat(settings-ui): add LocalizationExtensions class to connect localization library

---
+++ src/Settings/Playhouse.Settings.UI/Services/Localization/LocalizationExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using KebabGGbab.Localization;
+using Microsoft.Extensions.DependencyInjection;
+using Playhouse.Settings.UI.Resources;
+
+namespace Playhouse.Settings.UI.Services.Localization
+{
+    public static class LocalizationExtensions
+    {
+        public static IServiceCollection AddLocalizationSettings(this IServiceCollection services)
+        {
+            ArgumentNullException.ThrowIfNull(services);
+
+            services.AddLocalization();
+            services.AddResxLocalization(StringsUI.ResourceManager, [CultureInfo.GetCultureInfo("en-US"), CultureInfo.GetCultureInfo("ru-RU")]);
+
+            return services;
+        }
+    }
+}
