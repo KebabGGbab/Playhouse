2efefaac354baa40cf14c114a4f275890ee42f83
artem <kebabggbab@gmail.com>
2026-05-29 22:29:33 +0700

    feat(shared-domain): add details in error

--- src/SharedKernel/Playhouse.SharedKernel.Domain/Results/Error.cs
+++ src/SharedKernel/Playhouse.SharedKernel.Domain/Results/Error.cs
@@ -2,9 +2,13 @@
 {
     public abstract class Error
     {
+        private static readonly IReadOnlyDictionary<string, object> _emptyDetails = new Dictionary<string, object>().AsReadOnly();
+
         public string Code { get; }
 
         public string Message { get; }
+
+        public IReadOnlyDictionary<string, object> Details { get; }
 
         protected Error(string message, string code)
         {
@@ -13,6 +17,15 @@
 
             Message = message;
             Code = code;
+            Details = _emptyDetails;
+        }
+
+        protected Error(string message, string code, IReadOnlyDictionary<string, object> details)
+            : this(message, code)
+        {
+            ArgumentNullException.ThrowIfNull(details);
+
+            Details = details;
         }
     }
 }
