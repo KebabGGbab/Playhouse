aaddce1273dc9965f919235396aa125e93e7fb5c
artem <kebabggbab@gmail.com>
2026-05-30 09:17:52 +0700

    feat(settings-application): add dto for ApplicationSettings class

---
+++ src/Settings/Playhouse.Settings.Application/DTO/ApplicationSettingsDto.cs
@@ -0,0 +1,22 @@
+﻿namespace Playhouse.Settings.Application.DTO
+{
+    public class ApplicationSettingsDto
+    {
+        public string CultureName { get; }
+
+        public string PathToData { get; }
+
+        public IReadOnlyList<string> Browsers { get; }
+
+        public IReadOnlyList<string> Channels { get; }
+
+        public ApplicationSettingsDto(string cultureName, string pathToData, 
+            IEnumerable<string> addedBrowsers, IEnumerable<string> addedChannels)
+        {
+            CultureName = cultureName;
+            PathToData = pathToData;
+            Browsers = addedBrowsers.ToList().AsReadOnly();
+            Channels = addedChannels.ToList().AsReadOnly();
+        }
+    }
+}
