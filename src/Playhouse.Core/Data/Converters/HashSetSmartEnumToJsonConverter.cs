dea8b6983598eb81bd4779cffdae1994fc721c6f
artem <kebabggbab@gmail.com>
2026-06-04 22:32:06 +0700

    fix: fixed bugs in SetToJsonConverter
    SetToJsonConverter has been renamed to HashSetSmartEnumToJsonConverter and works with the ISet interface.
    
    In HashSetSmartEnumToJsonConverter.        public static ISet<TEnum> ConvertFromJson(string json) added default value if collection is null

---
+++ src/Playhouse.Core/Data/Converters/HashSetSmartEnumToJsonConverter.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json;
+using Ardalis.SmartEnum;
+using Ardalis.SmartEnum.EFCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace Playhouse.Core.Data.Converters
+{
+    internal class HashSetSmartEnumToJsonConverter<TEnum, TValue> : ValueConverter<ISet<TEnum>, string>
+        where TEnum : SmartEnum<TEnum, TValue>
+        where TValue : IEquatable<TValue>, IComparable<TValue>
+    {
+        public HashSetSmartEnumToJsonConverter() 
+            : base(
+                  v => ConvertToJson(v),
+                  v => ConvertFromJson(v))
+        {
+        }
+
+        public static string ConvertToJson(ISet<TEnum> enums)
+        {
+            return JsonSerializer.Serialize(enums.Select(e => e.Value));
+        }
+
+        public static ISet<TEnum> ConvertFromJson(string json)
+        {
+            ISet<TValue>? values = JsonSerializer.Deserialize<ISet<TValue>>(json);
+
+            if (values == null)
+            {
+                return new HashSet<TEnum>();
+            }
+
+            return values.Select(SmartEnumConverter<TEnum, TValue>.GetFromValue).ToHashSet();
+        }
+    }
+}
