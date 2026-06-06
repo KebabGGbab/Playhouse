6016a6e021ed977964e99d26849fde94c6c9234f
artem <kebabggbab@gmail.com>
2026-06-06 21:38:42 +0700

    fix(ui): design error in UpdateWindow

--- src/Playhouse.UI/Views/Windows/UpdateWindow.axaml.cs
+++ src/Playhouse.UI/Views/Windows/UpdateWindow.axaml.cs
@@ -6,15 +6,38 @@ namespace Playhouse.UI.Views.Windows;
 
 internal sealed partial class UpdateWindow : Window
 {
+    private readonly UpdateViewModel _vm = null!;
+
+    /// <summary>
+    /// Конструктор для дизайнера
+    /// </summary>
+    public UpdateWindow()
+    {
+        if (!Design.IsDesignMode)
+        {
+            throw new InvalidOperationException();
+        }
+
+        InitializeComponent();
+    }
+
     public UpdateWindow(UpdateViewModel vm)
     {
+        ArgumentNullException.ThrowIfNull(vm);
+
         DataContext = vm;
+        _vm = vm;
         InitializeComponent();
     }
 
     private async void Window_Loaded(object? sender, RoutedEventArgs e)
     {
-        await ((UpdateViewModel)DataContext!).InitializeCommand.ExecuteAsync(null);
+        if (Design.IsDesignMode)
+        {
+            return;
+        }
+
+        await _vm.InitializeCommand.ExecuteAsync(null);
         Close();
     }
 }
