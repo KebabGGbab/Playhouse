d86500216c6fd32859ef491cffdaf9b680ba3155
artem <kebabggbab@gmail.com>
2026-06-06 19:09:30 +0700

    refactoring(ui): WindowFactory
    A null check for dependencies has been added to the FileManagerService class constructors
    
    IBotConstructorFactory changed to IConstructorFactory

--- src/Playhouse.UI/Services/WindowCreatorService/WindowFactory.cs
+++ src/Playhouse.UI/Services/WindowCreatorService/WindowFactory.cs
@@ -1,4 +1,4 @@
-﻿using Playhouse.Core.Services.BotConstructorService.Abstractions;
+﻿using Playhouse.Core.Services.ConstructorService.Abstractions;
 using Playhouse.UI.Services.WindowCreatorService.Abstractions;
 using Playhouse.UI.Views.Windows;
 using Playhouse.ViewModels.ViewModels;
@@ -7,16 +7,19 @@ namespace Playhouse.UI.Services.WindowCreatorService
 {
     internal sealed class WindowFactory : IWindowFactory
     {
-        private readonly IBotConstructorFactory _botConstructorFactory;
+        private readonly IConstructorFactory _botConstructorFactory;
 
-        public WindowFactory(IBotConstructorFactory botConstructorFactory)
+        public WindowFactory(IConstructorFactory botConstructorFactory)
         {
+            ArgumentNullException.ThrowIfNull(botConstructorFactory);
+
             _botConstructorFactory = botConstructorFactory;
         }
 
         public BotConstructorWindow CreateBotConstructorWindow(BrowserConfigurationViewModel browser, BotConfigurationViewModel bot)
         {
             BotConstructorViewModel viewModel = new(_botConstructorFactory, browser, bot);
+
             return new BotConstructorWindow(viewModel);
         }
     }
