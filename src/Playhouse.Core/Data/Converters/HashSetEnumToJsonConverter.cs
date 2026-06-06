e6119735a5a03a4a09938c962a45ce8bf7376fa0
artem <kebabggbab@gmail.com>
2026-06-06 18:28:57 +0700

    feat(core): add convertors to json for enum and other types

---
+++ src/Playhouse.Core/Data/Converters/HashSetEnumToJsonConverter.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace Playhouse.Core.Data.Converters
+{
+    internal sealed class HashSetEnumToJsonConverter<TEnum> : ValueConverter<ISet<TEnum>, string>
+        where TEnum : struct
+    {
+        public HashSetEnumToJsonConverter()
+            : base(
+                  v => ConvertToJson(v),
+                  v => ConvertFromJson(v))
+        {
+        }
+
+        public static string ConvertToJson(ISet<TEnum> enums)
+        {
+            return JsonSerializer.Serialize(enums.Select(e => e.ToString()));
+        }
+
+        public static ISet<TEnum> ConvertFromJson(string json)
+        {
+            IEnumerable<string>? values = JsonSerializer.Deserialize<IEnumerable<string>>(json);
+
+            if (values is null)
+            {
+                return new HashSet<TEnum>();
+            }
+            
+            return values.Select(Enum.Parse<TEnum>).ToHashSet();
+        }
+    }
+}
---
+++ src/Playhouse.Core/Data/Converters/HashSetToJsonConverter.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace Playhouse.Core.Data.Converters
+{
+    internal class HashSetToJsonConverter<TItem> : ValueConverter<ISet<TItem>, string>
+    {
+        public HashSetToJsonConverter()
+            : base(
+                  v => ConvertToJson(v),
+                  v => ConvertFromJson(v))
+        {
+        }
+
+        public static string ConvertToJson(ISet<TItem> items)
+        {
+            return JsonSerializer.Serialize(items);
+        }
+
+        public static ISet<TItem> ConvertFromJson(string json)
+        {
+            return JsonSerializer.Deserialize<ISet<TItem>>(json) ?? new HashSet<TItem>();
+        }
+    }
+}
