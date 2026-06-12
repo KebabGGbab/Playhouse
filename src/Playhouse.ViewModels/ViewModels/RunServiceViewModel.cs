fd17b63af27993f1c04179bee20c41d6b4c617f0
artem <kebabggbab@gmail.com>
2026-06-12 10:23:04 +0700

    feat(viewmodels): add RunServiceViewModel and RunUnitServiceViewModel classes

---
+++ src/Playhouse.ViewModels/ViewModels/RunServiceViewModel.cs
@@ -0,0 +1,46 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Playhouse.Core.Services.BotRunningService;
+
+namespace Playhouse.ViewModels.ViewModels
+{
+    public sealed class RunServiceViewModel : ObservableObject
+    {
+        private readonly IRunService _runService;
+
+        public BotConfigurationViewModel Bot { get; }
+
+        public IReadOnlyCollection<RunUnitServiceViewModel> Units { get; }
+
+        public int Progress => _runService.Progress;
+
+        public event EventHandler<RunServiceViewModel, EventArgs>? RunCompleted;
+
+        public RunServiceViewModel(IRunService runService, BotConfigurationViewModel bot, IEnumerable<BrowserConfigurationViewModel> browsers)
+        {
+            ArgumentNullException.ThrowIfNull(runService);
+
+            _runService = runService;
+            _runService.ProgressChanged += RunServiceProgressChanged;
+            _runService.RunCompleted += OnRunServiceCompleted;
+            Bot = bot;
+            Units = browsers.Select(b => new RunUnitServiceViewModel(runService.Units.First(u => u.Browser == b.Profile), bot, b)).ToList().AsReadOnly();
+        }
+
+        public async Task RunAsync()
+        {
+            await _runService.RunAsync();
+        }
+
+        private void RunServiceProgressChanged(IRunService sender, EventArgs e)
+        {
+            OnPropertyChanged(nameof(Progress));
+        }
+
+        private void OnRunServiceCompleted(IRunService sender, EventArgs e)
+        {
+            RunCompleted?.Invoke(this, EventArgs.Empty);
+            _runService.ProgressChanged -= RunServiceProgressChanged;
+            _runService.RunCompleted -= OnRunServiceCompleted;
+        }
+    }
+}
---
+++ src/Playhouse.ViewModels/ViewModels/RunUnitServiceViewModel.cs
@@ -0,0 +1,28 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Playhouse.Core.Services.BotRunningService;
+
+namespace Playhouse.ViewModels.ViewModels
+{
+    public sealed class RunUnitServiceViewModel : ObservableObject
+    {
+        private readonly IRunUnit _runUnit;
+
+        public BotConfigurationViewModel Bot { get; }
+
+        public BrowserConfigurationViewModel Browser { get; }
+
+        public string Status => _runUnit.Status.ToString();
+
+        public RunUnitServiceViewModel(IRunUnit unit, BotConfigurationViewModel bot, BrowserConfigurationViewModel browser)
+        {
+            ArgumentNullException.ThrowIfNull(unit);
+            ArgumentNullException.ThrowIfNull(bot);
+            ArgumentNullException.ThrowIfNull(browser);
+
+            _runUnit = unit;
+            _runUnit.StatusChanged += (sender, e) => OnPropertyChanged(nameof(Status));
+            Bot = bot;
+            Browser = browser;
+        }
+    }
+}
