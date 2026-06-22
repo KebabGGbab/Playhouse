3a3c640e10e125631b22d514231091f2095c4aa3
artem <kebabggbab@gmail.com>
2026-06-22 23:22:27 +0700

    feat(application): add formatter for text with variable

---
+++ src/Playhouse.Application/Services/VariableService/VariableFormatter.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+using Playhouse.Domain;
+
+namespace Playhouse.Application.Services.VariableService
+{
+    internal static partial class VariableFormatter
+    {
+        public static string Format(string format, IEnumerable<Variable> variables)
+        {
+            ArgumentNullException.ThrowIfNull(format);
+            ArgumentNullException.ThrowIfNull(variables);
+
+            return VariableRegex().Replace(format, (match) =>
+            {
+                Variable? variable = variables.FirstOrDefault(v => v.Name == match.Groups["Name"].Value);
+
+                return variable == null ? match.Value : variable.Value;
+            });
+        }
+
+        [GeneratedRegex(@"\${(?<Name>[^{}]+)}")]
+        private static partial Regex VariableRegex();
+    }
+}
